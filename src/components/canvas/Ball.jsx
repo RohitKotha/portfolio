77b720a820a4739ad535c7d5405785af223d542a
rohit k <22MH1A42G9@acoe.edu.in>
2025-03-06 00:40:57 +0530

    first commit

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -17,7 +17,7 @@ const Ball = (props) => {
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
-        <icosahedronGeometry args={[1, 2]} />
+        <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial
           color="#3d3d3d"
           polygonOffset
@@ -37,9 +37,9 @@ const Ball = (props) => {
 
 const BallCanvas = ({ icon }) => {
   return (
-    <Canvas frameloop="always" gl={{ preserveDrawingBuffer: true }}>
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }} >
       <Suspense fallback={<Loader />}>
-        <OrbitControls enableZoom={false} position0={0} />
+        <OrbitControls enableZoom={false}/>
         <Ball imgUrl={icon} />
       </Suspense>
 
